dbee3d88ffbdf84a51bd96d5b3c49f89b0f654b2
Stefano Di Cecco <s.dicecco@reply.it>
2021-02-25 19:53:14 +0100

    added social icons

--- src/components/Blog/Auth.tsx
+++ src/components/Blog/Auth.tsx
@@ -9,23 +9,38 @@ import {
 } from "@react-firebase/auth";
 import { config } from "../../config";
 import { Button } from "@material-ui/core";
-import GTranslateIcon from '@material-ui/icons/GTranslate';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import GTranslateIcon from '@material-ui/icons/GTranslate';
 
 function Auth() {
   return (
     <FirebaseAuthProvider {...config} firebase={firebase}>
         <Button
-          style={{ marginRight: 10, backgroundColor: '#DC4C39', color: '#fff' }}
+          style={{ marginRight: 10, backgroundColor: '#EA4336' }}
           variant="outlined"
-          startIcon={<GTranslateIcon/>}
           onClick={() => {
             const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
             firebase.auth().signInWithPopup(googleAuthProvider);
           }}
-        >
-          Continue with Google
-        </Button>
+        ><GTranslateIcon /></Button>
+        <Button
+          style={{ marginRight: 10, backgroundColor: '#2D88FF' }}
+          variant="outlined"
+          onClick={() => {
+            const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
+            firebase.auth().signInWithPopup(facebookAuthProvider);
+          }}
+        ><FacebookIcon /></Button>
+        <Button
+          style={{ marginRight: 10, backgroundColor: '#1B91DA' }}
+          variant="outlined"
+          onClick={() => {
+            const twitterAuthProvider = new firebase.auth.TwitterAuthProvider();
+            firebase.auth().signInWithPopup(twitterAuthProvider);
+          }}
+        ><TwitterIcon /></Button>       
         <FirebaseAuthConsumer>
           {({ isSignedIn, user, providerId }) => {
             return (
